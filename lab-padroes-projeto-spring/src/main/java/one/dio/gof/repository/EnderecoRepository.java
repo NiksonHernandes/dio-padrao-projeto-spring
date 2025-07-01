1885381ddb3d39cfd7f234e67a5e24206b48373b
Nikson Hernandes <Nik.Hernandes14@hotmail.com>
2025-07-01 11:47:40 -0300

    feat: add aplicacao

---
+++ lab-padroes-projeto-spring/src/main/java/one/dio/gof/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package one.dio.gof.repository;
+
+import one.dio.gof.model.Endereco;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+}
